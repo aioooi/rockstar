9385dafd86ab48c25e8cfa07335cdfd1d8b60b0b
chris@t530 <66511731+aioooi@users.noreply.github.com>
2021-08-09 21:09:01 +0200

    add Cobra example

---
+++ examples/cobra_rockstar.py
@@ -0,0 +1,8 @@
+from rockstar import RockStar
+
+cobra_code = """class HelloWorld
+    def main
+        print 'Hello, World!'"""
+        
+rock_it_bro = RockStar(days=400, file_name='hello.cobra', code=cobra_code)
+rock_it_bro.make_me_a_rockstar()
